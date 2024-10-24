d92b3089515975c3f19f0a198366d83ef5ebcf1b
Mhmd Al Moussawi <mohamadalmoussawi@coolglitch.net>
2024-10-24 15:29:18 +0300

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Color Palette Plugin
+
+📽 [Showcase Video](https://youtu.be/UALGL8z6fvQ) <br/>
+📖 [Documentation](https://M-Cool-Glitch.github.io/ColorPalette-Documentation) <br/>
+🛰 [Discord](https://discord.coolglitch.net) <br/>
+
+**Color Palette Plugin** is a powerful plugin designed to streamline color management in Unreal Engine 5. 
+It provides an editor utility widget for managing multiple color palettes, making it easy to organize, import, 
+and export palettes. Share your colors with team members and access them seamlessly while working on your project.
+
+**Features:**
+- Create custom color palettes for easy organization
+- Assign unique names to each palette
+- Add customizable titles to individual colors
+- Edit palette names and remove specific colors as needed
+- Easily share color palettes with team members
+- Export palettes with selected colors for external use
+- Import palettes with control over which colors to include
---
+++ docusaurus.config.ts
@@ -0,0 +1,115 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Color Palette Plugin',
+  tagline: 'Easily import, export, and collaborate on palettes with your team',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://M-Cool-Glitch.github.io',
+  baseUrl: '/ColorPalette-Documentation/',
+
+  // GitHub pages deployment config.
+  organizationName: 'M-Cool-Glitch', // Usually your GitHub org/user name.
+  projectName: 'ColorPalette-Documentation', // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Color Palette',
+      logo: {
+        alt: 'Color Palette Logo',
+        src: 'img/ColorPalette-icon.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+
+        {to: "/changelog", label: "Changelog", position: "right"},
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: '/docs/getting-started',
+            },
+              
+            {
+              label: 'Changelog',
+              to: '/changelog',
+            },
+              
+          ],
+        },
+        {
+          title: "FAB",
+          items: [
+            {
+              label: "Color Palette Plugin",
+              href: "com.epicgames.launcher://ue/Fab/product/44d4ba43-10ad-453b-a83f-0203391cf2c9",
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.coolglitch.net',
+            }
+          ],
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Cool Glitch OU.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
