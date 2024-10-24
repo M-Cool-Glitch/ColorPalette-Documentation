fbb64953d0d5709a581f83b6d009fd04fc9f7377
Mhmd Al Moussawi <mohamadalmoussawi@coolglitch.net>
2024-10-24 18:01:08 +0300

    fix link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
               <Link
                   className="button button--secondary button--lg"
-                  to="/docs/getting-started">
+                  to="/docs/introduction">
                   Learn more
               </Link>
           </div>
